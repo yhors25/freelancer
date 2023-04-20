a8f0b4c959d4841a4360c4ee0459555cf5a19770
yhors25 <130920904+yhors25@users.noreply.github.com>
2023-04-20 17:03:58 -0500

    formReactive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'freelancer';
+  autor="Yhors Brayan"
+
+  constructor(private fb:FormBuilder){  }
+
+  contacto=this.fb.group({
+    nombre:['',Validators.required],
+    telefono:['',Validators.required],
+    correo:['',[Validators.required,Validators.email]],
+    mensaje:[''],
+    
+  })
+  
+  guardarContacto(){
+    console.log(this.contacto.value);
+  }
+
 }
